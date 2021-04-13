0fb2c1f2c1c091d9c8babd9ec08cef81c3da2d19
Andrei Shkarin <a.shkarin@solvemate.com>
2021-04-14 01:43:20 +0200

    Processor for parallel processing + Unit tests

---
+++ processor/processor.go
@@ -0,0 +1,99 @@
+package processor
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Value string
+
+var NoValue Value = ""
+
+type Result struct {
+	Input  Value
+	Output Value
+	Error  error
+}
+
+type Processor func(Value) Result
+
+// Process processes values with processor using nWorkers and returns
+// an unordered generator of ProcessingResult
+func Process(values chan Value, processor Processor, nWorkers uint, queueSize uint) (<-chan Result, error) {
+	if nWorkers == 0 {
+		return nil, fmt.Errorf("nWorkers should be greater or equal to 1")
+	}
+
+	results := make(chan Result)
+	workersQueues := make(chan (chan<- Value), nWorkers)
+	workersResults := make(chan (<-chan Result), nWorkers)
+	wg := new(sync.WaitGroup)
+	wg.Add(int(nWorkers))
+
+	// spawn workers
+	go func() {
+		var i uint
+		for i = 0; i < nWorkers; i++ {
+			workerQueue := make(chan Value, queueSize)
+			workersQueues <- workerQueue
+			workersResults <- worker(workerQueue, processor)
+		}
+	}()
+
+	// aggregate results
+	go func() {
+		for {
+			workerResults, opened := <-workersResults
+			if !opened {
+				return
+			}
+
+			result, opened := <-workerResults
+			if !opened {
+				wg.Done()
+				continue
+			}
+
+			workersResults <- workerResults
+			results <- result
+		}
+	}()
+
+	// finalize
+	go func() {
+		wg.Wait()
+		close(workersQueues)
+		close(workersResults)
+		close(results)
+	}()
+
+	// distribte values
+	go func() {
+		for value := range values {
+			workerQueue := <-workersQueues
+			workerQueue <- value
+			workersQueues <- workerQueue
+		}
+
+		for workerQueue := range workersQueues {
+			close(workerQueue)
+		}
+	}()
+
+	return results, nil
+}
+
+func worker(values <-chan Value, processor Processor) <-chan Result {
+	results := make(chan Result)
+	go func() {
+		for {
+			value, opened := <-values
+			if !opened {
+				close(results)
+				return
+			}
+			results <- processor(value)
+		}
+	}()
+	return results
+}
---
+++ processor/processor_test.go
@@ -0,0 +1,51 @@
+package processor_test
+
+import (
+	p "http-response-hasher/processor"
+	"testing"
+)
+
+func appendHello(value p.Value) p.Result {
+	return p.Result{
+		Input:  value,
+		Output: p.Value(string(value) + "Hello"),
+		Error:  nil,
+	}
+}
+
+func generateValues() (map[string]string, chan p.Value) {
+	data := []string{"cat", "dog", "rat", "truck", ""}
+	reference := make(map[string]string)
+	values := make(chan p.Value, len(data))
+
+	for _, v := range data {
+		values <- p.Value(v)
+		reference[v] = v + "Hello"
+	}
+	close(values)
+
+	return reference, values
+}
+
+func TestProcess(t *testing.T) {
+	var nWorkers uint
+	for nWorkers = 1; nWorkers < 10; nWorkers++ {
+		reference, valuesQueue := generateValues()
+		results, _ := p.Process(valuesQueue, appendHello, nWorkers, 1)
+		for result := range results {
+			if reference[string(result.Input)] != string(result.Output) {
+				t.Errorf("Processor returned wrong result: %v != %v", result.Input, reference[string(result.Input)])
+			}
+		}
+	}
+
+	nWorkers = 0
+	_, valuesQueue := generateValues()
+	results, err := p.Process(valuesQueue, appendHello, nWorkers, 1)
+	if err == nil {
+		t.Error("Processor should return an error when nWorkers=0")
+	}
+	if results != nil {
+		t.Error("Processor should not return a generator when nWorkers=0")
+	}
+}
