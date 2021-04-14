b8c795e72085da773c0e045741e92901b27fce9c
Andrei Shkarin <a.shkarin@solvemate.com>
2021-04-14 02:17:38 +0200

    Remove dependency on urlx

--- app/app.go
+++ app/app.go
@@ -1,19 +1,30 @@
 package app
 
 import (
-	"net/http"
-
-	"github.com/goware/urlx"
 	"http-response-hasher/hasher"
 	p "http-response-hasher/processor"
+	"net/http"
+	"net/url"
+	"strings"
 )
+
+func parseUrl(rawUrl string, defaultScheme string) (*url.URL, error) {
+	if strings.Index(rawUrl, "//") == 0 {
+		rawUrl = defaultScheme + ":" + rawUrl
+	}
+	if !strings.Contains(rawUrl, "://") {
+		rawUrl = defaultScheme + "://" + rawUrl
+	}
+
+	return url.ParseRequestURI(rawUrl)
+}
 
 // requestUrlAndHashResponse performs a GET request to url and returns
 // a processor.Result instance, which Input is url and Output is an MD5
 // hash of the response body.
 func requestUrlAndHashResponse(value p.Value) p.Result {
 	rawUrl := string(value)
-	url, err := urlx.Parse(rawUrl)
+	url, err := parseUrl(rawUrl, "http")
 	if err != nil {
 		return p.Result{
 			Input:  p.Value(rawUrl),
